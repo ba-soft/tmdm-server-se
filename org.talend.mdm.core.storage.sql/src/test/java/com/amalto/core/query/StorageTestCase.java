73e6ff3afba48c2497fe948b5f0f5df4ccad068a
Victor Li <vli@talend.com>
2020-09-03 10:19:18 +0800

    test performance issue.

--- org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/query/StorageTestCase.java
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/query/StorageTestCase.java
@@ -170,12 +170,6 @@ public class StorageTestCase extends TestCase {
 
     protected static final ComplexTypeMetadata refRegion;
 
-    protected static final ComplexTypeMetadata orgActivity;
-
-    protected static final ComplexTypeMetadata orgPerson;
-
-    protected static final ComplexTypeMetadata orgEntity;
-
     public static final String DATABASE = "H2";
 
     public static final String DATASOURCE_DEFAULT = DATABASE + "-Default";
@@ -255,10 +249,6 @@ public class StorageTestCase extends TestCase {
 
         refPays = repository.getComplexType("RefPays");
         refRegion = repository.getComplexType("RefRegion");
-
-        orgActivity = repository.getComplexType("OrgActivity");
-        orgPerson = repository.getComplexType("OrgPerson");
-        orgEntity = repository.getComplexType("OrgEntity");
 
         systemStorage = new SecuredStorage(new HibernateStorage("MDM", StorageType.SYSTEM), userSecurity);
         systemRepository = buildSystemRepository();
