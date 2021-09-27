19536f28430ea4a743effca5ee897813a7fbb5c9
huawei.zhu <41101497+hw-zhu@users.noreply.github.com>
2021-09-27 17:29:31 +0800

    TMDM-15174 Error when deploying a new version of Data Model : HHH000388: Unsuccessful: alter table xxx add constraint FK_yyy foreign key (x_contacts_x_talend_id) references X_ANONYMOUS9 (#1396)
    * TMDM-13276 [Impact Analysis] ElementType change (length) not reflected in PostgreSQL database tables 
    * TMDM-13214 Default value expression in MetaDataRepository
    * TMDM-14099 Search complex Foreign Key Field with "is empty or null" throws error
    * TMDM-15174 error when deploying a new version of Data Model : HHH000388: Unsuccessful: alter table xxx add constraint FK_yyy foreign key (x_contacts_x_talend_id) references X_ANONYMOUS9

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/mapping/MDMTableUtils.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/mapping/MDMTableUtils.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
- * 
+ * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+ *
  * This source code is available under agreement available at
  * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
- * 
+ *
  * You should have received a copy of the agreement along with this program; if not, write to Talend SA 9 rue Pages
  * 92150 Suresnes, France
  */
@@ -11,42 +11,80 @@
 package com.amalto.core.storage.hibernate.mapping;
 
 import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.PostgreSQLDialect;
 import org.hibernate.dialect.SQLServerDialect;
 import org.hibernate.mapping.Column;
 import org.hibernate.tool.hbm2ddl.ColumnMetadata;
 
+import java.sql.Types;
+
 @SuppressWarnings("nls")
 public abstract class MDMTableUtils {
 
-    public static final String NO = "NO";
+    public static final String NVARCHAR_MAX_TYPE = "nvarchar(max)";
+
+    private static final String NO = "NO";
 
     public static boolean isAlterColumnField(Column newColumn, ColumnMetadata oldColumnInfo, Dialect dialect) {
         if (oldColumnInfo == null) {
             return Boolean.FALSE;
         }
-        boolean isNeedToAlter = Boolean.TRUE;
-        isNeedToAlter &= isVarcharField(newColumn, oldColumnInfo, dialect)
-                && isIncreaseVarcharColumnLength(newColumn, oldColumnInfo, dialect);
-        return isNeedToAlter;
+        return isVarcharField(oldColumnInfo, dialect) && isIncreaseVarcharColumnLength(newColumn, oldColumnInfo, dialect)
+                || isVarcharTypeChanged(newColumn, oldColumnInfo, dialect);
     }
 
-    public static boolean isVarcharField(Column newColumn, ColumnMetadata oldColumnInfo, Dialect dialect) {
-        boolean isVarcharType = oldColumnInfo.getTypeCode() == java.sql.Types.VARCHAR;
+    private static boolean isVarcharTypeChanged(Column newColumn, ColumnMetadata oldColumnInfo, Dialect dialect) {
+        if(!isVarcharField(oldColumnInfo, dialect)){
+            return false;
+        }
+        if (dialect instanceof PostgreSQLDialect) {
+            return oldColumnInfo.getTypeName().toLowerCase().startsWith("varchar") && newColumn.getSqlType().equalsIgnoreCase("text");
+        }
         if (dialect instanceof SQLServerDialect) {
-            isVarcharType |= oldColumnInfo.getTypeCode() == java.sql.Types.NVARCHAR
+            String newColumnType = newColumn.getSqlType();
+            String oldColumnType = oldColumnInfo.getTypeName();
+
+            // oldColumnType=nvarchar and the oldColumnInfo length=Integer.MAX_VALUE, oldColumnType changed to nvarchar(max)
+            if (oldColumnType.equals("nvarchar") && oldColumnInfo.getColumnSize() == Integer.MAX_VALUE) {
+                oldColumnType = NVARCHAR_MAX_TYPE;
+            }
+
+            // newColumnType=nvarchar(200), newColumnType changed to nvarchar
+            if (!newColumnType.equals(NVARCHAR_MAX_TYPE) && newColumnType.contains("(")) {
+                newColumnType = newColumnType.substring(0, newColumnType.indexOf('('));
+            }
+            // oldColumnType=nvarchar(200), oldColumnType changed to nvarchar
+            if (!oldColumnType.equals(NVARCHAR_MAX_TYPE) && oldColumnType.contains("(")) {
+                oldColumnType = oldColumnType.substring(0, oldColumnType.indexOf('('));
+            }
+            return !newColumnType.equals(oldColumnType);
+        }
+
+        return (oldColumnInfo.getTypeCode() == Types.VARCHAR || oldColumnInfo.getTypeCode() == Types.NVARCHAR) && (
+                newColumn.getSqlTypeCode() == Types.LONGVARCHAR || newColumn.getSqlTypeCode() == Types.CLOB);
+    }
+
+
+    private static boolean isVarcharField(ColumnMetadata oldColumnInfo, Dialect dialect) {
+        boolean isVarcharType = oldColumnInfo.getTypeCode() == Types.VARCHAR;
+        if (dialect instanceof SQLServerDialect) {
+            isVarcharType |= oldColumnInfo.getTypeCode() == Types.NVARCHAR
                     && oldColumnInfo.getTypeName().equalsIgnoreCase("nvarchar");
         }
         return isVarcharType;
     }
 
-    public static boolean isIncreaseVarcharColumnLength(Column newColumn, ColumnMetadata oldColumnInfo, Dialect dialect) {
-        return newColumn.getLength() > oldColumnInfo.getColumnSize();
+    private static boolean isIncreaseVarcharColumnLength(Column newColumn, ColumnMetadata oldColumnInfo, Dialect dialect) {
+        if (dialect instanceof SQLServerDialect) {
+            return newColumn.getLength() > oldColumnInfo.getColumnSize() && (oldColumnInfo.getTypeCode() == Types.NVARCHAR
+                    || oldColumnInfo.getTypeCode() == Types.VARCHAR) && (newColumn.getSqlTypeCode() == Types.NVARCHAR
+                    || newColumn.getSqlTypeCode() == Types.VARCHAR);
+        }
+        return newColumn.getLength() > oldColumnInfo.getColumnSize() && (newColumn.getSqlTypeCode() == oldColumnInfo
+                .getTypeCode());
     }
 
     public static boolean isChangedToOptional(Column newColumn, ColumnMetadata oldColumnInfo) {
-        if (oldColumnInfo.getNullable().toUpperCase().equals(NO) && newColumn.isNullable()) {
-            return true;
-        }
-        return false;
+        return oldColumnInfo.getNullable().toUpperCase().equals(NO) && newColumn.isNullable();
     }
 }
